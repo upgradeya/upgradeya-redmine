899444d1b4071dddb93ea0932eda40038c6a745b
David Parrish <daveparrish@gmail.com>
2014-06-27 22:27:30 -0400

    Add script to inspect running passenger container

--- scripts/inspect_passenger.sh
+++ scripts/inspect_passenger.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-container_name=$(fig ps | grep figredmine_passenger_1 | awk '{print $1}')
-container_ip=$(docker ps | grep $container_name | awk '{print $1}' | xargs docker inspect -f '{{ .NetworkSettings.IPAddress }}')
+container_ip=$(docker ps | grep 'figredmine_passenger_1 ' | awk '{print $1}' | xargs docker inspect -f '{{ .NetworkSettings.IPAddress }}')
 
 # Current directory
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
