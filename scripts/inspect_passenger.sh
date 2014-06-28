6ac7300e28d9c06c2c25b1984239143c7b242490
David Parrish <daveparrish@gmail.com>
2014-06-27 23:27:53 -0400

    Improve correctness of regex to find the passenger containers IP

--- scripts/inspect_passenger.sh
+++ scripts/inspect_passenger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-container_ip=$(docker ps | grep 'figredmine_passenger_1 ' | awk '{print $1}' | xargs docker inspect -f '{{ .NetworkSettings.IPAddress }}')
+container_ip=$(docker ps | grep -e 'p[ ]*figredmine_passenger_1[ ]*$' | awk '{print $1}' | xargs docker inspect -f '{{ .NetworkSettings.IPAddress }}')
 
 # Current directory
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
