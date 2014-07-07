a3b2a80a39e52f7aff394b7cefb31b9ad8203c8f
David Parrish <daveparrish@gmail.com>
2014-07-07 03:25:32 -0400

    Properly set RAILS_ENV when configuring Redmine database. Seperate each rake command for easier debugging.

--- scripts/initialize_redmine.sh
+++ scripts/initialize_redmine.sh
@@ -40,5 +40,11 @@ echo "Creating Redmine user"
 fig run --rm mariadb mysql -u super -p$mariadb_super_password -h $mariadb_container_ip -e 'CREATE USER '"'"'redmine'"'"'@'"'"'172.%'"'"' IDENTIFIED BY '"'"$password"'"''
 fig run --rm mariadb mysql -u super -p$mariadb_super_password -h $mariadb_container_ip -e 'GRANT ALL PRIVILEGES ON redmine_production.* TO '"'"'redmine'"'"'@'"'"'172.%'"'"''
 
-echo "Creating Redmine database and initializing with default data"
-fig run --rm passenger bash -c 'cd /home/app/redmine; RAILS_ENV=production rake db:create && RAILS_ENV=production rake db:migrate && rake generate_secret_token && RAILS_ENV=production rake redmine:load_default_data'
+echo "Creating Redmine database"
+fig run --rm passenger bash -c 'cd /home/app/redmine; (export RAILS_ENV=production && rake db:create)'
+echo "Migrating database"
+fig run --rm passenger bash -c 'cd /home/app/redmine; (export RAILS_ENV=production && rake db:migrate)'
+echo "Generating secret token"
+fig run --rm passenger bash -c 'cd /home/app/redmine; rake generate_secret_token'
+echo "Loading default data"
+fig run --rm passenger bash -c 'cd /home/app/redmine; (export RAILS_ENV=production && rake redmine:load_default_data)'
