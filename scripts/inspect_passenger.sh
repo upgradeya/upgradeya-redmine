e66664bb6e3a39d04323dd6509b8daa39df969e0
David Parrish <daveparrish@gmail.com>
2014-07-07 04:11:52 -0400

    Allow inspect_passenger to work in any root project directory Fix insecure_key permissions before trying to ssh

--- scripts/inspect_passenger.sh
+++ scripts/inspect_passenger.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-container_ip=$(docker ps | grep -e 'p[ ]*figredmine_passenger_1[ ]*$' | awk '{print $1}' | xargs docker inspect -f '{{ .NetworkSettings.IPAddress }}')
+container_ip=$(fig ps -q passenger | xargs docker inspect -f '{{ .NetworkSettings.IPAddress }}')
 
 # Current directory
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Set correct permissions on insecure_key
+chmod 600 $dir/insecure_key
 
 ssh -i $dir/insecure_key root@$container_ip
