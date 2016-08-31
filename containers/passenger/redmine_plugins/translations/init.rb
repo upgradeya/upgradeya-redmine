e79b05501e473526642a688b2b0e4f89ca7ffee3
David Parrish <daveparrish@tutanota.com>
2016-08-31 09:08:53 -0400

    Add changes required for Redmine version 3.3-stable

--- containers/passenger/redmine_plugins/translations/init.rb
+++ containers/passenger/redmine_plugins/translations/init.rb
@@ -2,8 +2,8 @@ Redmine::Plugin.register :translations do
   name 'Translations'
   author 'Wesley Jones'
   description 'Fake plugin to fix missing translations.'
-  version '1.0.0'
+  version '1.0.1'
 
-  requires_redmine :version => '3.0'..'3.2'
+  requires_redmine :version => '3.0'..'3.3'
 
 end
